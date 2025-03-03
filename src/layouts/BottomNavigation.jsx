829025f23face46bfb23a014a7d2fffcbfe5cea4
pouriasabaghi <pouriasabaghi@gmail.com>
2025-03-03 10:03:43 +0330

    🎨 design:  animate navbar icons

--- src/layouts/BottomNavigation.jsx
+++ src/layouts/BottomNavigation.jsx
@@ -1,23 +1,50 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import { useNetworkStatus } from "@/context/NetworkStatusContext";
 import { toast } from "sonner";
-import {
-  HomeSVG,
-  PlaylistsSVG,
-  FavoritesOutlineSVG,
-  SearchSVG,
-} from "@/ui/Icons";
 
 import Logo from "@/assets/icons/logo.svg";
+import { useRive, useStateMachineInput } from "@rive-app/react-canvas";
+import { useEffect, useState } from "react";
 
 function BottomNavigation() {
+  const location = useLocation();
+  console.log(location.pathname);
+
+  const [activeIcon, setActiveIcon] = useState(null);
+
   const isOffline = useNetworkStatus();
 
-  function handleClick(e) {
+  const navLinks = [
+    {
+      icon: "home",
+      link: "/",
+      text: "Home",
+    },
+    {
+      icon: "playlist",
+      link: "/playlists",
+      text: "Playlists",
+    },
+    {
+      icon: "zoom",
+      link: "/explore",
+      text: "Explore",
+    },
+    {
+      icon: "star",
+      link: "/favorites",
+      text: "Favorites",
+    },
+  ];
+
+  function handleClick(e, icon) {
     if (isOffline) {
       e.preventDefault();
       toast.warning("Not available in offline mode");
+      return;
     }
+
+    setActiveIcon(icon);
   }
 
   return (
@@ -26,41 +53,58 @@ function BottomNavigation() {
         <img width="35" height="35" src={Logo} alt="Myplaylists" />
         <span className="text-base font-extrabold">Myplaylists</span>
       </div>
-
-      <NavigationItem link="/" icon={<HomeSVG />} text="Home" />
-
-      <NavigationItem
-        link="/playlists"
-        icon={<PlaylistsSVG />}
-        text="Playlists"
-      />
-
-      <NavigationItem
-        link="/explore"
-        icon={<SearchSVG size={35} />}
-        text="Explore"
-        onClick={handleClick}
-      />
-
-      <NavigationItem
-        link="/favorites"
-        icon={<FavoritesOutlineSVG />}
-        text="Favorites"
-        onClick={handleClick}
-      />
+      {navLinks.map((item) => (
+        <NavigationItem
+          key={item.icon}
+          onClick={handleClick}
+          icon={item.icon}
+          link={item.link}
+          text={item.text}
+          activeIcon={activeIcon}
+          activePath={location.pathname}
+        />
+      ))}
     </nav>
   );
 }
 
-function NavigationItem({ link, icon, text, onClick, isOffline }) {
-  return (<NavLink
-    to={link}
-    onClick={() => onClick?.()}
-    className={`flex flex-col items-center py-3 xl:flex-row xl:mb-2 xl:w-full xl:justify-start xl:gap-x-3 ${isOffline ? "opacity-20" : ""}`}
-  >
-    {icon}
-    <span className="mx-auto mt-1 text-[10px] xl:m-0 xl:text-lg">{text}</span>
-  </NavLink>)
+function NavigationItem({
+  link,
+  icon,
+  text,
+  onClick,
+  isOffline,
+  activeIcon,
+  activePath,
+}) {
+  const { rive, RiveComponent } = useRive({
+    src: `/src/assets/${icon}.riv`,
+    stateMachines: "click",
+    autoplay: true,
+    artboard: icon.charAt(0).toUpperCase() + icon.substring(1),
+    shouldDisableRiveListeners: true,
+  });
+
+  const isActive = useStateMachineInput(rive, "click", "Active");
+
+  useEffect(() => {
+    if (isActive) {      
+      isActive.value = activeIcon === icon || activePath === link;
+    }
+  }, [isActive, activeIcon, icon, activePath, link]);
+
+  return (
+    <NavLink
+      to={link}
+      onClick={(e) => onClick?.(e, icon)}
+      className={`flex flex-col items-center py-3 first-letter:flex xl:mb-2 xl:w-full xl:flex-row xl:justify-start xl:gap-x-3 ${isOffline ? "opacity-20" : ""}`}
+    >
+      <div className="h-9 w-9">
+        <RiveComponent />
+      </div>
+      <span className="mx-auto mt-1 text-[10px] xl:m-0 xl:text-lg">{text}</span>
+    </NavLink>
+  );
 }
 
 export default BottomNavigation;
