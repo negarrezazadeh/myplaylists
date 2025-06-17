9531bc4687422cdeb284e9f817708f7abe165c4f
rznegrr <negarrz1378@gmail.com>
2025-06-17 15:06:02 +0330

    🧑‍💻 chore: add fallback images for banner and cover

--- src/features/profile/ProfileInfo.jsx
+++ src/features/profile/ProfileInfo.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import useGetUserProfile from "./useGetUserProfile";
 import FullPageSpinner from "@/ui/FullPageSpinner";
 import PlaylistItem from "../playlist/PlaylistItem";
+import { Link } from "react-router-dom";
+import myplaylistsCover from "../../assets/img/no-cover-logo.png";
+import defaultBanner from "../../assets/img/defaultBanner.jpeg";
 
 export default function ProfileInfo({ userId }) {
   const { isLoading, profile } = useGetUserProfile(userId);
@@ -13,23 +16,28 @@ export default function ProfileInfo({ userId }) {
       {/* banner section */}
       <div className="relative -top-10 right-0 h-44 w-full md:h-64">
         <img
-          src={profile.banner}
+          src={!profile.banner ? defaultBanner : profile.banner}
           alt="user banner"
           className="h-full w-full rounded-b-2xl object-cover"
         />
         <div className="absolute inset-0 rounded-b-2xl bg-black/50 lg:rounded-br-none" />
+        <Link to="/edit-profile" className="absolute inset-0">
+          Add Banner
+        </Link>
       </div>
 
       {/* user section */}
       <div className="relative left-5 -mt-[115px] flex w-full items-center gap-x-3 md:-mt-[130px]">
         <img
-          src={profile.avatar}
+          src={!profile.avatar ? myplaylistsCover : profile.avatar}
           alt="user"
           className="h-24 w-24 rounded-full ring-2 ring-gray-600 md:h-32 md:w-32"
         />
-        <div className="mb-7 flex flex-col">
+        <div className="mb-7 flex flex-col gap-y-1">
           <h4 className="text-2xl font-bold capitalize">{profile.name}</h4>
-          <p className="text-sm text-gray-400">{profile.bio}</p>
+          <p className="line-clamp-2 w-64 text-sm font-semibold text-gray-300 lg:w-full">
+            {!profile.bio ? "" : profile.bio}
+          </p>
         </div>
       </div>
       <div className="mt-8 grid">
