c3e5635f1a542adc3488d59e0a2e8f1ced9ae014
pouriasabaghi <pouriasabaghi@gmail.com>
2025-07-25 22:11:25 +0330

    chore: remove loader for subscribe button

--- src/features/subscription/Subscribe.jsx
+++ src/features/subscription/Subscribe.jsx
@@ -1,13 +1,10 @@
 import { useSubscribe } from "./useSubscribe";
 import useIsSubscribe from "./useIsSubscribe";
-import FullPageSpinner from "@/ui/FullPageSpinner";
 import { Button } from "@/ui/button";
 
 export default function Subscribe({ userId, userIdParam }) {
   const { subscribe, isPending } = useSubscribe();
   const { isSubscribe, isLoading } = useIsSubscribe(userIdParam);
-
-  if (isLoading) return <FullPageSpinner />;
 
   const handleSubscribe = () => {
     subscribe(userId);
@@ -16,12 +13,12 @@ export default function Subscribe({ userId, userIdParam }) {
   return (
     <Button
       onClick={handleSubscribe}
-      disabled={isPending}
-      className={
+      disabled={isPending || isLoading}
+      className={`text-white ${
         isSubscribe
-          ? "bg-gray-700 text-white hover:bg-gray-600"
-          : "bg-purple-500 text-white hover:bg-purple-400"
-      }
+          ? "bg-gray-700 hover:bg-gray-600"
+          : "bg-purple-500 hover:bg-purple-400"
+      } `}
     >
       {isSubscribe ? "Unsubscribe" : "Subscribe"}
     </Button>
