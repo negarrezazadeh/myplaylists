f0ae97928d8fda7bfb7d6977947ce2a048ea6982
rznegrr <negarrz1378@gmail.com>
2025-08-12 23:11:12 +0330

    🧑‍💻 chore: add subscriber profile link

--- src/features/subscription/SubscribeItem.jsx
+++ src/features/subscription/SubscribeItem.jsx
@@ -3,7 +3,10 @@ import myPlaylistsCover from "../../assets/img/no-cover-logo.png";
 
 export default function SubscribeItem({ sub }) {
   return (
-    <div className="flex w-full items-start gap-x-3 border-b border-black py-4 last:border-b-0">
+    <Link
+      to={`/profile/${sub.id}`}
+      className="flex w-full items-start gap-x-3 border-b border-black py-4 last:border-b-0"
+    >
       <img
         src={sub.avatar ? sub.avatar : myPlaylistsCover}
         alt="user-avatar"
@@ -15,6 +18,6 @@ export default function SubscribeItem({ sub }) {
           {sub.bio}
         </p>
       </div>
-    </div>
+    </Link>
   );
 }
