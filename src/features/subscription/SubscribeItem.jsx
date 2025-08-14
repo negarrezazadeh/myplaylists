6f0191bc45fd16f593a90c2306acd0d2a917e0da
rznegrr <negarrz1378@gmail.com>
2025-08-14 22:26:22 +0330

    🧑‍💻 chore: fix subscribeItem issue

--- src/features/subscription/SubscribeItem.jsx
+++ src/features/subscription/SubscribeItem.jsx
@@ -7,19 +7,26 @@ export default function SubscribeItem({ sub }) {
   const { user } = useAuth();
 
   return (
-    <div className="flex w-full items-center gap-x-3 border-b border-black py-4 last:border-b-0">
-      <Link to={`/profile/${sub.id}`} className="flex w-full gap-x-3">
+    <div className="grid w-full grid-cols-6 items-center gap-x-3 border-b border-black py-4 last:border-b-0 xl:grid-cols-12">
+      <Link
+        to={`/profile/${sub.id}`}
+        className={`flex w-full gap-x-3 xl:col-span-10 ${user.id !== sub.id ? "col-span-4" : "col-span-6"}`}
+      >
         <img
           src={sub.avatar ? sub.avatar : myPlaylistsCover}
           alt="user-avatar"
           className="h-16 w-16 rounded-full"
         />
-        <div className="flex w-52 flex-col gap-y-1 overflow-hidden xl:w-[800px]">
+        <div className="flex flex-col gap-y-1 overflow-hidden">
           <p className="mt-1 text-lg font-semibold">{sub.name}</p>
-          <p className="w-full truncate text-sm text-gray-300">{sub.bio}</p>
+          <p className="truncate text-sm text-gray-300">{sub.bio}</p>
         </div>
       </Link>
-      <Subscribe userId={sub.id} userIdParam={user.id} />
+      <div className="col-span-2 place-self-center xl:col-span-2">
+        {user.id !== sub.id && (
+          <Subscribe userId={sub.id} userIdParam={user.id} />
+        )}
+      </div>
     </div>
   );
 }
